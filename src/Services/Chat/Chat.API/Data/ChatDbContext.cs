710519338230e2d51dcc306fddabf26be5ada820
endless001 <zze@live.com>
2021-10-16 20:53:12 +0800

    update

--- src/Services/Chat/Chat.API/Data/ChatDbContext.cs
+++ src/Services/Chat/Chat.API/Data/ChatDbContext.cs
@@ -8,24 +8,13 @@ namespace Chat.API.Data
     public class ChatDbContext
     {
         private readonly IMongoDatabase _database;
-        private readonly MongoClient _client;
 
         public ChatDbContext(IOptions<MongoConnection> options)
         {
-            _client = new MongoClient(options.Value.ConnectionString);
-            if (_client != null)
-            {
-                _database = _client.GetDatabase(options.Value.Database);
-            }
+          var client = new MongoClient(options.Value.ConnectionString);
+          _database = client.GetDatabase(options.Value.Database);
         }
 
-        public IMongoCollection<Record> Records
-        {
-            get
-            {
-                return _database.GetCollection<Record>("Record");
-            }
-        }
-      
+        public IMongoCollection<Record> Records => _database.GetCollection<Record>("Record");
     }
 }
