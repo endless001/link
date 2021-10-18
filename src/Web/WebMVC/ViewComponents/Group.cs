59898423759ce3f17e524151cf1e63e41e90e443
qing long <zze@live.com>
2021-10-18 13:34:56 +0800

    update

---
+++ src/Web/WebMVC/ViewComponents/Group.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace WebMVC.ViewComponents
+{
+    public class Group: ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            await Task.CompletedTask;
+            return View();
+        }
+    }
+}
