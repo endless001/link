40db3add0c8b9260caba959234161f4b5ee0a950
qing long <zze@live.com>
2021-10-23 14:41:42 +0800

    update

--- src/Services/Account/Account.API/Infrastructure/Providers/CustomEncryptionProvider.cs
+++ src/Services/Account/Account.API/Infrastructure/Providers/CustomEncryptionProvider.cs
@@ -4,37 +4,37 @@ using System.Text;
 
 namespace Account.API.Infrastructure.Providers
 {
-  public class CustomEncryptionProvider:IEncryptionProvider
-  {
-    private const string Key = "cxz92k13md8f981hu6y7alkc";
-    public string Encrypt(string value)
+    public class CustomEncryptionProvider : IEncryptionProvider
     {
-      var inputs = Encoding.UTF8.GetBytes(value);
-      var tripleDes = new TripleDESCryptoServiceProvider
-      {
-        Key = Encoding.UTF8.GetBytes(Key),
-        Mode = CipherMode.ECB,
-        Padding = PaddingMode.PKCS7
-      };
-      var transform = tripleDes.CreateEncryptor();
-      var results = transform.TransformFinalBlock(inputs, 0, inputs.Length);
-      tripleDes.Clear();
-      return Convert.ToBase64String(results, 0, results.Length);
-    }
+        private const string Key = "cxz92k13md8f981hu6y7alkc";
+        public string Encrypt(string value)
+        {
+            var inputs = Encoding.UTF8.GetBytes(value);
+            var tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            var transform = tripleDes.CreateEncryptor();
+            var results = transform.TransformFinalBlock(inputs, 0, inputs.Length);
+            tripleDes.Clear();
+            return Convert.ToBase64String(results, 0, results.Length);
+        }
 
-    public string Decrypt(string value)
-    {
-      var inputs = Convert.FromBase64String(value);
-      var tripleDes = new TripleDESCryptoServiceProvider
-      {
-        Key = Encoding.UTF8.GetBytes(Key),
-        Mode = CipherMode.ECB,
-        Padding = PaddingMode.PKCS7
-      };
-      var transform = tripleDes.CreateDecryptor();
-      var results = transform.TransformFinalBlock(inputs, 0, inputs.Length);
-      tripleDes.Clear();
-      return Encoding.UTF8.GetString(results);
+        public string Decrypt(string value)
+        {
+            var inputs = Convert.FromBase64String(value);
+            var tripleDes = new TripleDESCryptoServiceProvider
+            {
+                Key = Encoding.UTF8.GetBytes(Key),
+                Mode = CipherMode.ECB,
+                Padding = PaddingMode.PKCS7
+            };
+            var transform = tripleDes.CreateDecryptor();
+            var results = transform.TransformFinalBlock(inputs, 0, inputs.Length);
+            tripleDes.Clear();
+            return Encoding.UTF8.GetString(results);
+        }
     }
-  }
 }
